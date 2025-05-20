55b9a4d010e49aa76aecf47fe1a985b054a25297
IvaneHo <10608@holbertonstudents.com>
2025-05-20 10:15:22 +0200

    102

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -1,1 +1,63 @@
 #!/usr/bin/python3
+"""Defines a Square class that can be printed."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position with validation."""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(n, int) for n in value) or
+                not all(n >= 0 for n in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square using `#` and respecting position."""
+        if self.__size == 0:
+            print()
+            return
+
+        print("\n" * self.__position[1], end="")
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """Return the square as a string using `#`."""
+        if self.__size == 0:
+            return ""
+
+        result = [""] * self.__position[1]
+        for _ in range(self.__size):
+            line = " " * self.__position[0] + "#" * self.__size
+            result.append(line)
+        return "\n".join(result)

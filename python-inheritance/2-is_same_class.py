b373d6b63927ad5161077bbc5af4766897e7c447
IvaneHo <10608@holbertonstudents.com>
2025-05-27 08:13:30 +0200

    2 espace pour pycode

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module checks if an object is exactly an instance of a given class."""
 
+
 def is_same_class(obj, a_class):
     """Check if obj is exactly an instance of a_class."""
     return type(obj) is a_class

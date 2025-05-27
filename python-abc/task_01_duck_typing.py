f7e83ef680e76c5d0d5af8d826677e565d0db15d
IvaneHo <10608@holbertonstudents.com>
2025-05-27 18:10:17 +0200

    check 8 passe pas

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -17,11 +17,7 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
-        if not isinstance(radius, (int, float)):
-            raise TypeError("radius must be a number")
-        if radius <= 0:
-            raise ValueError("radius must be > 0")
-        self.__radius = radius
+        self.__radius = abs(radius)
 
     def area(self):
         return math.pi * self.__radius ** 2
@@ -32,12 +28,8 @@ class Circle(Shape):
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        if not all(isinstance(x, (int, float)) for x in (width, height)):
-            raise TypeError("width and height must be numbers")
-        if width <= 0 or height <= 0:
-            raise ValueError("width and height must be > 0")
-        self.__width = width
-        self.__height = height
+        self.__width = abs(width)
+        self.__height = abs(height)
 
     def area(self):
         return self.__width * self.__height

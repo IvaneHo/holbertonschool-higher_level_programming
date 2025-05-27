0d651c609144359928d04330f32cd4e1331fa896
IvaneHo <10608@holbertonstudents.com>
2025-05-27 17:22:03 +0200

    Correction 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -2,39 +2,31 @@
 """Abstract class Shape, with Circle and Rectangle subclasses using duck typing."""
 
 from abc import ABC, abstractmethod
-from math import pi
+import math
 
 
 class Shape(ABC):
-    """Abstract base class for geometric shapes."""
-
     @abstractmethod
     def area(self):
-        """Must return area of shape"""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Must return perimeter of shape"""
         pass
 
 
 class Circle(Shape):
-    """Circle shape, defined by its radius."""
-
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        return pi * self.radius ** 2
+        return math.pi * self.radius ** 2
 
     def perimeter(self):
-        return 2 * pi * self.radius
+        return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """Rectangle shape, defined by width and height."""
-
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -46,7 +38,7 @@ class Rectangle(Shape):
         return 2 * (self.width + self.height)
 
 
-def shape_info(obj):
-    """Prints area and perimeter of any object with matching methods (duck typing)."""
-    print("Area:", obj.area())
-    print("Perimeter:", obj.perimeter())
+def shape_info(shape):
+    print("Area: {:.15f}".format(shape.area()))
+    print("Perimeter: {:.15f}".format(shape.perimeter()))
+

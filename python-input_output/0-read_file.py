8ff7754e438a07557d431ae69b551b6d498e1b49
IvaneHo <10608@holbertonstudents.com>
2025-06-02 15:58:51 +0200

    moins de 79 chars

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """Module for reading and printing the contents of a UTF-8 text file."""
 
+
 def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its content to stdout without extra newline."""
+    """
+    Reads a UTF-8 text file and prints its content to stdout
+    without adding extra newline at the end.
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")

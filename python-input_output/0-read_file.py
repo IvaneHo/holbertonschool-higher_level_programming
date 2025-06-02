cedd9d18ca5e2fdc317ac07df64f8159db906c40
IvaneHo <10608@holbertonstudents.com>
2025-06-02 15:55:07 +0200

    Exo 0 correction

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# Fonction pour lire un fichier texte (UTF8) et afficher le contenu dans la console
+"""Module for reading and printing the contents of a UTF-8 text file."""
 
 def read_file(filename=""):
-    """Affiche le contenu du fichier texte donné (UTF-8) sans ajouter de saut de ligne supplémentaire."""
+    """Reads a UTF-8 text file and prints its content to stdout without extra newline."""
     with open(filename, encoding="utf-8") as f:
-        content = f.read()
-        print(content, end="")
+        print(f.read(), end="")

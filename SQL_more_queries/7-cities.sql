029e09d06d14b539a8ae4656f13e983dec1f83d3
IvaneHo <10608@holbertonstudents.com>
2025-06-23 09:56:16 +0200

    1 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Create database hbtn_0d_usa and table cities with foreign key to states.id
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);

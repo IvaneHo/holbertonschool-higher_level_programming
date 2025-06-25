34b2862684a3b337a249077b48c46b353a3a9f66
IvaneHo <10608@holbertonstudents.com>
2025-06-25 12:44:08 +0200

    FIN

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,9 @@
--- Converts hbtn_0c_0 and its table/column to UTF8 (utf8mb4, utf8mb4_unicode_ci)
+-- Transform a DB and table to UTF-8 format
+-- Select the correct DataBase
 USE hbtn_0c_0;
 
+-- DataBase transformation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Table transformation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;

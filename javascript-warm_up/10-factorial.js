efe1320b935a1792f81e655a4667f098a415a3a9
IvaneHo <10608@holbertonstudents.com>
2025-07-11 22:40:31 +0200

    THE END 2.0

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+
 function factorial (n) {
-  if (isNaN(n) || n === 0) return 1;
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
   return n * factorial(n - 1);
 }
 
-const arg = parseInt(process.argv[2]);
-console.log(factorial(arg));
+const num = parseInt(argv[2]);
+console.log(factorial(num));
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+
 function add (a, b) {
   return a + b;
 }
 
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
+const a = parseInt(argv[2]);
+const b = parseInt(argv[3]);
 console.log(add(a, b));

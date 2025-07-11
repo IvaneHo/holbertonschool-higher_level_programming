52771ffdead52c6ea161f28ae2f552df98e30dd0
IvaneHo <10608@holbertonstudents.com>
2025-07-11 22:43:13 +0200

    99999

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
 function add (a, b) {
   return a + b;
 }
 
-const a = parseInt(argv[2]);
-const b = parseInt(argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 console.log(add(a, b));
+

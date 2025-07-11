ce70d1c01167cc75f97bc06de803b34c9fe4dfd9
IvaneHo <10608@holbertonstudents.com>
2025-07-11 22:44:44 +0200

    dd

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
 function factorial (n) {
-  if (isNaN(n)) {
-    return 1;
-  }
-  if (n === 0 || n === 1) {
-    return 1;
-  }
+  if (isNaN(n) || n === 0) return 1;
   return n * factorial(n - 1);
 }
 
-const num = parseInt(argv[2]);
-console.log(factorial(num));
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));
